bb10218708a60e3db6cacf081193561e7b4c7749
Y P <yp@Yos-MacBook-Pro.local>
2021-01-27 02:49:16 +0900

    11 commit

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,13 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+const name = 'YOSY POKARI'
 
-export default function Layout({ children, home }) {
+export default function Page() {
     return (
         <div className={styles.container}>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <meta
-                    name="description"
-                    content="YOSY POKARI"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.now.sh/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
+
             </Head>
             <header className={styles.header}>
                 {home ? (
@@ -37,7 +26,6 @@ export default function Layout({ children, home }) {
                         </>
                     )}
             </header>
-            <main>{children}</main>
 
         </div>
     )

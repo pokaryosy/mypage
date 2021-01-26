26a4581f6914f8b1e858fb64d27cd6224337bc6b
Y P <yp@Yos-MacBook-Pro.local>
2021-01-27 00:58:47 +0900

    4 commit

---
+++ pages/_document.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Page = (props) => (
+  <div>
+    <Head>
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+    </Head>
+    // Other layout/components
+  </div>
+);
+
+export default Page;

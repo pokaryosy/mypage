6071417efbff957b1818b0a5c56dde39c9fca8b7
Y P <yp@Yos-MacBook-Pro.local>
2021-01-27 03:06:17 +0900

    18 commit

--- pages/index.js
+++ pages/index.js
@@ -23,13 +23,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
           <Link href="/about.js">
             <a className={utilStyles.colorInherit}>about.</a>
           </Link>
           {' '}
           <a href="https://www.instagram.com/pokaryosy/">instagram.</a>
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>

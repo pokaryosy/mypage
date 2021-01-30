4b18cf5b80ad1eb0e51e407104084ce6de26cf86
Y P <yp@Yos-MacBook-Pro.local>
2021-01-31 04:04:37 +0900

    175 commits

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home({ allPostsData }) {
       </section>
 
       <footer className={utilStyles.footer}>
-        <p>© 2021 YOSY POKARI</p>
+        <p><span>© 2021 YOSY POKARI</span></p>
       </footer>
 
     </Layout>

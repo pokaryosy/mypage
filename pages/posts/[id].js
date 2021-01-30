33c274221575dbdcf3efe7b4ed37783142c933d2
Y P <yp@Yos-MacBook-Pro.local>
2021-01-30 23:36:42 +0900

    130 commits

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import Link from 'next/link'
 
 
 export async function getStaticProps({ params }) {

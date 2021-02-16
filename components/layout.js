2433ba99900948dc7901a64ced51fcd4c16148fd
Y P <yp@Yos-MacBook-Pro.local>
2021-02-16 16:03:10 +0900

    commits

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="musician | performer | model | software UI/UX engineer, Tp. + F.Hr. + DJ + Vo"
+          content="musician | model | filmmaker | software UI/UX engineer, Tp. + F.Hr. + DJ + Vo + Fl"
         />
         <meta
           property="og:image"
@@ -23,8 +23,9 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:site" content="@pokaryosy" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@pokaryosy" />
+        <meta property="og:image" content="/images/profile.jpg" />
       </Head>
       <header className={styles.header}>
         {home ? (

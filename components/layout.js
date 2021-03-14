1806594171b708bc3e1994c0d116e59bf79f4978
Y P <yp@Yos-MacBook-Pro.local>
2021-03-14 22:44:04 +0900

    commits

--- components/layout.js
+++ components/layout.js
@@ -25,7 +25,7 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@pokaryosy" />
-        <meta property="og:image" content="/images/profile.jpg" />
+        <meta property="og:image" content="https://www.pokaryosy.com/about/profile.jpg" />
       </Head>
       <header className={styles.header}>
         {home ? (

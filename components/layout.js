2e27824d82f23a4e4bddcd6a72fa56d6ddb24214
Y P <yp@Yos-MacBook-Pro.local>
2021-03-19 01:16:23 +0900

    commits

--- components/layout.js
+++ components/layout.js
@@ -26,6 +26,8 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@pokaryosy" />
         <meta property="og:image" content="https://www.pokaryosy.com/images/minori2020/minori2020_1.jpg" />
+        <meta property="fb:app_id" content="486043602775575" />
+        
       </Head>
       <header className={styles.header}>
         {home ? (

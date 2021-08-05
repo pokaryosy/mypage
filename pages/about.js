5ef064b8dcfab50b8c206cc8efebc862567cd206
Y P <pokaryosy@gmail.com>
2021-08-05 12:16:09 +0900

    change1

--- pages/about.js
+++ pages/about.js
@@ -8,103 +8,137 @@ import Layout, { siteTitle } from '../components/layout'
 const name = 'YOSY POKARI'
 
 export default function Page({ children, home }) {
-    return (
-        <Layout home>
-            <div>
-                <Head>
-                    <title>about.</title>
-                </Head>
-                <header>
+    return ( <
+        Layout home >
+        <
+        div >
+        <
+        Head >
+        <
+        title > about. < /title> <
+        /Head> <
+        header >
 
-                </header>
-                <section className={utilStyles.headingMd}>
-                    <Link href="/about">
-                        <a className={utilStyles.colorInherit}>about.</a>
-                    </Link>&emsp;
-                <a href="https://linkco.re/0CsC8Z6T?lang=ja">music.</a>&emsp;
-                <a href="https://www.instagram.com/pokaryosy/">instagram.</a>&emsp;
-                <a href="https://twitter.com/pokaryosy">twitter.</a>&emsp;
-                <a href="https://suzuri.jp/YOSY">items.</a>&emsp;
-                <a href="https://github.com/pokaryosy">github.</a>&emsp;
+        <
+        /header> <
+        section className = { utilStyles.headingMd } >
+        <
+        Link href = "/about" >
+        <
+        a className = { utilStyles.colorInherit } > about. < /a> <
+        /Link>&emsp; <
+        a href = "https://linkco.re/0CsC8Z6T?lang=ja" > music. < /a>&emsp; <
+        a href = "https://www.instagram.com/pokaryosy/" > instagram. < /a>&emsp; <
+        a href = "https://twitter.com/pokaryosy" > twitter. < /a>&emsp; <
+        a href = "https://suzuri.jp/YOSY" > items. < /a>&emsp; <
+        a href = "https://github.com/pokaryosy" > github. < /a>&emsp;
 
-            </section>
-                <section>
-                    <body>
-                        <br />
-                        <p>
-                            <img
-                                src="/images/profile.jpg"
-                                className={`${utilStyles.artistphoto}`}
-                                alt={name}
-                                align="right"
-                            />
-                        musician <br />
-                        model <br />
-                        filmmaker <br />
-                        UI/UX designer <br />
-                        software engineer <br />
-                            <br />
-                        Tp. + F.Hr. + DJ + Vo + Fl
-                        </p>
+        <
+        /section> <
+        section >
+        <
+        body >
+        <
+        br / >
+        <
+        p >
+        <
+        img src = "/images/profile.jpg"
+        className = { `${utilStyles.artistphoto}` }
+        alt = { name }
+        align = "right" /
+        >
+        musician < br / >
+        artist < br / >
+        model < br / >
+        UI / UX designer < br / >
+        software engineer < br / >
+        <
+        br / >
+        Tp. + F.Hr. + DJ + Vo + Fl <
+        /p>
 
-                        <p className={utilStyles.clearcase}>
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                            <br />
-                            more details.
-                            <br />
-                            <br />
-                            <br />
-                            Growing up in London, Hong Kong, Tokyo and Okinawa.
-                            <br />
-                            <br />
-                            <br />
+        <
+        p className = { utilStyles.clearcase } >
+        <
+        br / >
+        <
+        br / >
+        <
+        br / >
+        <
+        br / >
+        <
+        br / >
+        more details. <
+        br / >
+        <
+        br / >
+        <
+        br / >
+        Growing up in London, Hong Kong, Tokyo and Okinawa. <
+        br / >
+        <
+        br / >
+        <
+        br / >
 
-                                        Awards
-                                        <br />
-                                        Keio University, Keio Light Music Society Jr. band (Bigband) <br />
-                                        - Stellar Jam ~ International Jazz Orchestra Festival ~ <br />
-                                        -- Unique Selection Champion and Nominee 7 (2018) <br />
-                                        -- Nominee 7 (2017) <br />
-                                        The 40th Kyushu Ensemble Contest 2015 Silver Award <br />
-                                        The 59th Kyushu brass band competition 2014 Gold Award <br />
-                                        Okinawa Junior High School 7th all-star Brass Band Principal Trumpet <br />
-                            <br />
-                            <br />
-                                        Skills
-                                        <br />
-                                        Programming Languages / Environments:  JavaScript, HTML, CSS, NoSQL, MySQL, Java, C#, C++, Python, Ruby on Rails, Processing, Max/MSP, Pure Data, MATLAB, Arduino, R, vvvv <br />
-                                        Software / Hardware:  Ableton Live, Final Cut Pro, Adobe XD / Illustrator, openFrameworks, Unity, Sibelius, Serato DJ Pro, synapseWear, MobMuPlat, OpenBCI, Wekinator, Pix4Dcapture <br />
-                                        Languages:  Japanese - native;  English - fluent;  Spanish - basic <br />
-                            <br />
-                            <br />
-                                        Education <br />
-                                        Keio University - B.A. (Environment and Information Studies) &emsp;2017 - <br />
-                                        Major: Computing and Music <br />
-                                        Research Theme: Weather music (New Music, New Performance with AI) <br />
-                            <br />
-                                        Goldsmiths, University of London - Study Abroad &emsp;2019 - 2020 <br />
-                                        Major: Computing and Music (Jazz performance and composition) <br />
-                                        TOBITATE! Young Ambassador Program (Japanese public-private Scholarship) <br />
+        Awards <
+        br / >
+        Keio University, Keio Light Music Society Jr.band(Bigband) < br / >
+        -Stellar Jam~International Jazz Orchestra Festival~ < br / >
+        --Unique Selection Champion and Nominee 7(2018) < br / >
+        --Nominee 7(2017) < br / >
+        The 40 th Kyushu Ensemble Contest 2015 Silver Award < br / >
+        The 59 th Kyushu brass band competition 2014 Gold Award < br / >
+        Okinawa Junior High School 7 th all - star Brass Band Principal Trumpet < br / >
+        <
+        br / >
+        <
+        br / >
+        Skills <
+        br / >
+        Programming Languages / Environments: JavaScript, HTML, CSS, NoSQL, MySQL, Java, C#, C++, Python, Ruby on Rails, Processing, Max / MSP, Pure Data, MATLAB, Arduino, R, vvvv < br / >
+        Software / Hardware: Ableton Live, Final Cut Pro, Adobe XD / Illustrator, openFrameworks, Unity, Sibelius, Serato DJ Pro, synapseWear, MobMuPlat, OpenBCI, Wekinator, Pix4Dcapture < br / >
+        Languages: Japanese - native; English - fluent; Spanish - basic < br / >
+        <
+        br / >
+        <
+        br / >
+        Education < br / >
+        Keio University - B.A.(Environment and Information Studies) & emsp; 2017 - < br / >
+        Major: Computing and Music < br / >
+        Research Theme: Weather music(New Music, New Performance with AI) < br / >
+        <
+        br / >
+        Goldsmiths, University of London - Study Abroad & emsp; 2019 - 2020 < br / >
+        Major: Computing and Music(Jazz performance and composition) < br / >
+        TOBITATE!Young Ambassador Program(Japanese public - private Scholarship) < br / >
 
 
-                        </p>
+        <
+        /p>
 
-                    </body>
-                </section>
-                <main>{children}</main>
-            </div>
+        <
+        /body> <
+        /section> <
+        main > { children } < /main> <
+        /div>
 
-            <br />
-            <br />
-            <br />
-            <footer className={utilStyles.footer}>
-                <p>© 2021 YOSY POKARI</p>
-            </footer>
+        <
+        br / >
+        <
+        br / >
+        <
+        br / >
+        <
+        footer className = { utilStyles.footer } >
+        <
+        p > ©2021 YOSY POKARI < /p> <
+        /footer>
 
-        </Layout>
+        <
+        /Layout>
 
     )
 }

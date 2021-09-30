f990f8ac119e9a572b719d7409786a8543e3ceeb
Y P <pokaryosy@gmail.com>
2021-09-30 15:35:15 +0900

    change each page menu

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -37,9 +37,12 @@ export default function Post({ postData }) {
                 </Link>&emsp;
                 <a href="https://linkco.re/0CsC8Z6T?lang=ja">music.</a>&emsp;
                 <a href="https://www.instagram.com/pokaryosy/">instagram.</a>&emsp;
+                {/* <br class="sp" /> */}
                 <a href="https://twitter.com/pokaryosy">twitter.</a>&emsp;
                 <a href="https://suzuri.jp/YOSY">items.</a>&emsp;
-                <a href="https://github.com/pokaryosy">github.</a>&emsp;
+                <a href="mailto:pokaryosy@gmail.com">contact.</a>&emsp;
+                {/* <a href="https://github.com/pokaryosy">github.</a>&emsp; */}
+                
             </section>
             <article>
                 <br />

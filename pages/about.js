29cd30c5f655071adcb61f8794f65528e1676927
Y P <pokaryosy@gmail.com>
2021-10-13 17:12:01 +0900

    footer and blod

--- pages/about.js
+++ pages/about.js
@@ -65,7 +65,7 @@ export default function Page({ children, home }) {
                             <br />
                             <br />
 
-                                        <futomozi>Awards</futomozi>
+                                        <div class="futomozi">Awards</div>
                                         <br />
                                         Keio University, Keio Light Music Society Jr. band (Bigband) <br />
                                         - Stellar Jam ~ International Jazz Orchestra Festival ~ <br />
@@ -76,14 +76,14 @@ export default function Page({ children, home }) {
                                         Okinawa Junior High School 7th all-star Brass Band Principal Trumpet <br />
                             <br />
                             <br />
-                                        <futomozi>Skills</futomozi>
+                                        <div class="futomozi">Skills</div>
                                         <br />
                                         Programming Languages / Environments:  JavaScript, HTML, CSS, NoSQL, MySQL, Java, C#, C++, Python, Ruby on Rails, Processing, Max/MSP, Pure Data, MATLAB, Arduino, R, vvvv <br />
                                         Software / Hardware:  Ableton Live, Final Cut Pro, Adobe XD / Illustrator, openFrameworks, Unity, Sibelius, Serato DJ Pro, synapseWear, MobMuPlat, OpenBCI, Wekinator, Pix4Dcapture <br />
                                         Languages:  Japanese - native;  English - fluent;  Spanish - basic <br />
                             <br />
                             <br />
-                                        <futomozi>Education</futomozi>
+                                        <div class="futomozi">Education</div>
                                         <br />
                                         Keio University - B.A. (Environment and Information Studies) &emsp;2017 - <br />
                                         Major: Computing and Music <br />

45642c7c889660a3ecc939b78e9ec0e820cb3454
Y P <pokaryosy@gmail.com>
2021-10-13 19:42:12 +0900

    change about5

--- pages/about.js
+++ pages/about.js
@@ -79,11 +79,13 @@ export default function Page({ children, home }) {
                             <br />
                                         <strong>Skills</strong>
                                         <br />
-                                        &emsp;Programming Languages / Environments  <br /> 
+                                        Programming Languages / Environments  <br /> 
                                         JavaScript, HTML, CSS, NoSQL, MySQL, Java, C#, C++, Python, Ruby on Rails, Processing, Max/MSP, Pure Data, MATLAB, Arduino, R, vvvv <br />
-                                        &emsp;Software / Hardware <br /> 
+                                        <br /> 
+                                        Software / Hardware <br /> 
                                         Ableton Live, Final Cut Pro, Adobe XD / Illustrator, openFrameworks, Unity, Sibelius, Serato DJ Pro, synapseWear, MobMuPlat, OpenBCI, Wekinator, Pix4Dcapture <br />
-                                        &emsp;Languages <br /> 
+                                        <br /> 
+                                        Languages <br /> 
                                         Japanese - native;  English - fluent;  Spanish - basic <br />
                             <br />
                             <br />

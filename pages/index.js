04c6bac6d2b08cf98ed47acb12623154766fa5d7
Y P <pokaryosy@gmail.com>
2023-01-31 19:58:24 +0900

    edit 2022 to 2023

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home({ allPostsData }) {
 
         <br />
         <footer className={utilStyles.footer}>
-          <p>© 2023 YOSY</p>
+          <p>© 2022 YOSY</p>
         </footer>
       </section>
 

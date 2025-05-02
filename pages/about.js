3b3a812bf5b93accb14d159893eb98bbde6a484e
Y P <pokaryosy@gmail.com>
2025-05-02 11:50:49 +0900

    edit about

--- pages/about.js
+++ pages/about.js
@@ -57,7 +57,7 @@ export default function Page({ children, home }) {
                             <br />
                             <br />
                             <br />
-                            <a href="https://drive.google.com/file/d/1p71e-ga0D-gOKGhfuTW3nmtCVJ5Ab2v0/view"> - cv </a>
+                            <a href="https://drive.google.com/file/d/1hc9Y7NPlGTSVwMvnv4lVCPON0sRA0Gi9/view"> - cv </a>
                             <br />
                             <a href="https://drive.google.com/file/d/16FDeMqk9THN_FkUM9-VzeCtPSDf0H6Id/view"> - portfolio </a>
                             <br />

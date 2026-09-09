3def8fd6a930ecf9b197f5a3e955c6bacd8876d3
Y P <pokaryosy@gmail.com>
2026-09-09 21:00:21 +0900

    change font size

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"></meta>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className={utilStyles.navMenu}>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"></meta>
         <Link href="/about" className={utilStyles.colorInherit}>
           about.
